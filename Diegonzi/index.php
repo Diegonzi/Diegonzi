26cd5d5f300267bc94d93c99595c758737b018f1
Diegonzi <40671209+Diegonzi@users.noreply.github.com>
2018-06-28 16:28:26 -0300

    Add files via upload

--- Diegonzi/index.php
+++ Diegonzi/index.php
@@ -1,5 +1,4 @@
 <?php 
-
 	require "devuelve_productos.php";
 
 	$bus=$_GET ["buscar"];
@@ -14,22 +13,41 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
-	<title>Document</title>
-	<link rel="stylesheet" href="estilos.css"/>
+	<title>Productos</title>
+	<style>
+		h1{
+			font-family:candara;
+			font-size: 60px;
+		}
+		#productos{
+  			font-family: sans-serif;
+  			font-size: 20px;
+  			border: 2px solid black;
+  			width: 300px;
+  			background-color: #FFEFEB; 
+  			display: inline-block;
+  			margin-right: 10px;
+		}
+		p{
+			margin-left: 5px;
+
+		}
+
+	</style>
 </head>
 <body>
+	<h1>Productos</h1>
+
 	<?php 
 
 		foreach($array_productos as $elemento){
-			echo "<div id=productos>"	
-			 echo "<table><tr><td>";
-			 echo "CODIGO: " . $elemento['CODIGO']."</td><td>";
-			 echo "NOMBRE: " . $elemento['NOMBRE']."</td><td>";
-			 echo "PRECIO: " . $elemento['PRECIO']."</td><td>";
-			 echo "STOCK: " . $elemento['STOCK']."</td></tr></table>";
-			 echo "<br>";
-			 echo "<br>";
-			echo"</div>"
+			echo "<div id=productos>";	
+			
+			 echo "<p><strong>Codigo del producto: </strong>" . $elemento['CODIGO']."</p>";
+			 echo "<p><strong>Nombre:</strong> " . $elemento['NOMBRE']."</p>";
+			 echo "<p><strong>Precio:</strong> " . $elemento['PRECIO']."</p>";
+			 echo "<p><strong>Stock:</strong> " . $elemento['STOCK']."</p>";			 			 
+			echo"</div>";
 				}
 
 
